76bff60d818e36725e8fe75fd306ed7a4d6da5c9
pavithran pavin <pavithran6382@gmail.com>
2025-08-17 12:49:47 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="contai

--- src/components/project-gallery.tsx
+++ src/components/project-gallery.tsx
@@ -77,7 +77,7 @@ export function ProjectGallery({ files, projectSlug }: ProjectGalleryProps) {
 
           <div className={cn(
             view === 'grid' 
-                ? 'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8' 
+                ? 'grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6' 
                 : 'space-y-4 project-gallery-list'
           )}>
             {filteredFiles.map((file, index) => (
